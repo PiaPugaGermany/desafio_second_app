934cec68747cf83c11976860910def5d249856ae
lumijavi <basturn@gmail.com>
2019-07-01 21:42:52 -0400

    'fourth' commit, add email stong params

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_22_153850) do
+ActiveRecord::Schema.define(version: 2019_07_02_013406) do
 
   create_table "students", force: :cascade do |t|
     t.string "name"
     t.integer "age"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
   end
 
 end
